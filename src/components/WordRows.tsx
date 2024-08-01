c7b046eb9aaf9cd321fd5ece1df4dccd844db630
ta-02 <talal.gl2004@gmail.com>
2024-07-31 21:50:32 -0400

    added wordRow template

--- src/components/WordRows.tsx
+++ src/components/WordRows.tsx
@@ -1,3 +1,35 @@
-export const WordRows = () => {
-  return <div>WordRows</div>;
+import { LetterState, WORD_LENGTH } from "../utils/utils";
+
+export const WordRows = ({
+  letters = "",
+  result,
+  className,
+}: {
+  letters: string;
+  result?: LetterState[];
+  className?: string;
+}) => {
+  const lettersRemaining = WORD_LENGTH - letters.length;
+  const showLetters = letters
+    .split("")
+    .concat(Array(lettersRemaining).fill(""));
+
+  return (
+    <div className={className}>
+      {showLetters.map((c, i) => (
+        <CharBox key={i} value={c} state={result ? result[i] : undefined} />
+      ))}
+    </div>
+  );
+};
+
+const charStateStyles: { [key in LetterState]: string } = {
+  Miss: "bg-gray-800",
+  Present: "bg-yellow-500",
+  Match: "bg-green-500",
+};
+
+const CharBox = ({ value, state }: { value: string; state?: LetterState }) => {
+  const style = state === undefined ? "" : charStateStyles[state];
+  return <span className={style}>{value}</span>;
 };
