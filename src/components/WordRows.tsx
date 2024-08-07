9d348b7c7e61e6ea47d8095a01ca9bd7c42d4147
ta-02 <talal.gl2004@gmail.com>
2024-08-07 19:30:12 -0400

    fixed to wordrows

--- src/components/WordRows.tsx
+++ src/components/WordRows.tsx
@@ -12,7 +12,7 @@ export const WordRows = ({
   const paddedLetters = letters.padEnd(WORD_LENGTH);
 
   return (
-    <div className={`${className}`}>
+    <div className={`flex flex-row gap-4 bg-green ${className}`}>
       {paddedLetters.split("").map((c, i) => (
         <CharBox key={i} value={c} state={result ? result[i] : undefined} />
       ))}
@@ -21,12 +21,18 @@ export const WordRows = ({
 };
 
 const charStateStyles: { [key in LetterState]: string } = {
-  Miss: "bg-gray-800",
-  Present: "bg-yellow-500",
-  Match: "bg-green-500",
+  Miss: "bg-gray-800 border-gray-800",
+  Present: "bg-yellow-500 border-yellow-500",
+  Match: "bg-green-500 border-green-500",
 };
 
 const CharBox = ({ value, state }: { value: string; state?: LetterState }) => {
   const style = state === undefined ? "" : charStateStyles[state];
-  return <span className={`${style}`}>{value}</span>;
+  return (
+    <span
+      className={`flex items-center justify-center w-12 h-12 p-4 text-2xl font-bold text-gray-50 text-center before:inline-block before:content-['_'] uppercase border border-gray-500 ${style}`}
+    >
+      {value}
+    </span>
+  );
 };
